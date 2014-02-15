98a1c1c179df806d61e7b978b824ae75cb5cbef8
Rolandas Razma <rolandas@razma.lt>
2014-02-15 16:43:36 +0200

    cleanup

--- iOS App (Mod)/RRBee/main.m
+++ iOS App (Mod)/RRBee/main.m
@@ -1,18 +1,17 @@
 //
 //  main.m
-//  Bee
+//  RRBee
 //
 //  Created by Rolandas Razma on 16/08/2013.
 //  Copyright (c) 2013 Rolandas Razma. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "RRAppDelegate.h"
 
-#import "YPAppDelegate.h"
 
-int main(int argc, char *argv[])
-{
+int main( int argc, char *argv[] ) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YPAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RRAppDelegate class]));
     }
 }
